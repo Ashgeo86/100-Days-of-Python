d0276a02fd08b0ec05a7b59c4def6518b65a6c37
Ashgeo86 <177663865+Ashgeo86@users.noreply.github.com>
2026-06-19 14:31:57 +0100

    Day 4 heads and tails

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -1,7 +1,18 @@
 import random
-import my_module
 
-random_integer = random.randint(1, 10)
-print(random_integer)
+#random_integer = random.randint(1, 10)
+#print(random_integer)
 
-print(my_module.my_favourite_number)
+#random.random will randomly generate a number between 0.0 and 1.0
+random_number_0_to_1 = random.random()
+print(random_number_0_to_1)
+
+#random.uniform return a random floating point that are between a and b, inclusive of a and b
+random_float = random.uniform(1, 10)
+print(random_float)
+
+random_heads_or_tails = random.randint(0, 1)
+if random_heads_or_tails == 0:
+    print("Heads")
+else:
+    print("Tails")
