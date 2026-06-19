23ea365d443c70c712ef8c4f812e080cd8c7f35f
Ashgeo86 <177663865+Ashgeo86@users.noreply.github.com>
2026-06-19 14:59:12 +0100

    Day 4 banker roullette

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,23 @@
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+random_friends_name = random.randint(1, 5)
+
+if random_friends_name == 1:
+    print(friends[0])
+elif random_friends_name == 2:
+    print(friends[1])
+elif random_friends_name == 3:
+    print(friends[2])
+elif random_friends_name == 4:
+    print(friends[3])
+else:print(friends[4])
+
+
+#simplified way to do it!
+print(random.choice(friends))
+
+#or this way
+random_index = random.randint(1, 5)
+print(friends[random_index])
---
+++ Day 4/Lists/task.py
@@ -0,0 +1,15 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+#prints out the state in that number of the list (0,1,2 etc)
+print(states_of_america[2])
+
+#prints the last state
+print(states_of_america[-1])
+
+#amends the spelling of the state in the list
+states_of_america[1] = "Pencilvania"
+print(states_of_america)
+
+#adds a new state to the end of the list
+states_of_america.append("Angelaland")
+print(states_of_america)
